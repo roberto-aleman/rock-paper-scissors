fbd8dccbb62e88c73d98403724c2f9bd71cc7dee
Roberto Aleman <165515200+roberto-aleman@users.noreply.github.com>
2024-06-18 12:12:31 -0700

    feat: add computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello bert");
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  console.log("Random number:", randomNumber);
+
+  if (randomNumber <= 1) {
+    return "rock";
+  } else if (randomNumber <= 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+const computerChoice = getComputerChoice();
+console.log("Computer chose: ", computerChoice);
