4c7dd45d61663f0631a4bc8883010c8998abeab0
Roberto Aleman <165515200+roberto-aleman@users.noreply.github.com>
2024-06-25 17:16:38 -0700

    refactor: show choices, score, and winner on HTML page

--- script.js
+++ script.js
@@ -1,78 +1,106 @@
+// Game logic
 const gameChoices = ["rock", "paper", "scissors"];
 
-function playGame() {
-  let computerScore = 0;
-  let humanScore = 0;
+let computerScore = 0;
+let humanScore = 0;
 
-  while (humanScore < 5 && computerScore < 5) {
-    playRound();
+let humanChoice;
+
+function playRound() {
+  const computerChoice = getComputerChoice();
+
+  switch (humanChoice) {
+    case "rock":
+      if (computerChoice === "rock") {
+        tieScenario();
+      } else if (computerChoice === "paper") {
+        lossScenario();
+      } else {
+        winScenario();
+      }
+      break;
+    case "paper":
+      if (computerChoice === "rock") {
+        winScenario();
+      } else if (computerChoice === "paper") {
+        tieScenario();
+      } else {
+        lossScenario();
+      }
+      break;
+    case "scissors":
+      if (computerChoice === "rock") {
+        lossScenario();
+      } else if (computerChoice === "paper") {
+        winScenario();
+      } else {
+        tieScenario();
+      }
+      break;
+    default:
+      console.log("Invalid choice");
+      break;
   }
-  console.log("Player: %s to Computer: %s", humanScore, computerScore);
 
-  function playRound() {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
+  function winScenario() {
+    console.log("You win! %s beats %s", humanChoice, computerChoice);
+    humanScore++;
+  }
 
-    switch (humanChoice) {
-      case "rock":
-        if (computerChoice === "rock") {
-          tieScenario();
-        } else if (computerChoice === "paper") {
-          lossScenario();
-        } else {
-          winScenario();
-        }
-        break;
-      case "paper":
-        if (computerChoice === "rock") {
-          winScenario();
-        } else if (computerChoice === "paper") {
-          tieScenario();
-        } else {
-          lossScenario();
-        }
-        break;
-      case "scissors":
-        if (computerChoice === "rock") {
-          lossScenario();
-        } else if (computerChoice === "paper") {
-          winScenario();
-        } else {
-          tieScenario();
-        }
-        break;
-      default:
-        console.log("Invalid choice");
-        break;
-    }
+  function lossScenario() {
+    console.log("You lose! %s beats %s", computerChoice, humanChoice);
+    computerScore++;
+  }
 
-    function winScenario() {
-      console.log("You win! %s beats %s", humanChoice, computerChoice);
-      humanScore++;
-    }
-
-    function lossScenario() {
-      console.log("You lose! %s beats %s", computerChoice, humanChoice);
-      computerScore++;
-    }
-
-    function tieScenario() {
-      console.log("Tie!");
-    }
+  function tieScenario() {
+    console.log("Tie!");
   }
 }
 
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
-  console.log("Computer chose: ", gameChoices[randomNumber]);
   return gameChoices[randomNumber];
 }
 
-function getHumanChoice() {
-  let humanChoice = prompt("Choose between rock, paper, or scissors");
-  humanChoice = humanChoice.toLowerCase();
-  console.log("Player chose:", humanChoice);
-  return humanChoice;
+// UI
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    handleClick(button.id);
+  });
+});
+
+function handleClick(choice) {
+  if (humanScore < 5 && computerScore < 5) {
+    humanChoice = choice;
+    playRound();
+    updateResults();
+  }
 }
 
-playGame();
+function updateResults() {
+  const div = document.querySelector("div");
+
+  let results = div.querySelector(".results");
+  if (!results) {
+    results = document.createElement("div");
+    results.classList.add("results");
+    div.appendChild(results);
+  }
+
+  let text = results.querySelector("p");
+  if (!text) {
+    text = document.createElement("p");
+    results.appendChild(text);
+  }
+
+  text.textContent = `Player: ${humanScore} to Computer: ${computerScore}`;
+
+  if (humanScore === 5) {
+    text.append(document.createElement("br"));
+    text.append("Player is the winner");
+  } else if (computerScore === 5) {
+    text.append(document.createElement("br"));
+    text.append("Computer is the winner");
+  }
+}
